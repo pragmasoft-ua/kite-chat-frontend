38c700fc979d67a36eb09390738b408b89e86324
RomanenkoStud <pavlo.romanenko@nure.ua>
2023-09-20 20:02:29 +0300

    Create docs deployment workflow

--- packages/kite-chat-docs/astro.config.mjs
+++ packages/kite-chat-docs/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.k1te.chat',
   integrations: [starlight({
     title: 'Kite Chat',
     favicon: '/kite.svg',
