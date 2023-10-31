37b183ce2b4051bccbbf872e95d49a34437f06a2
RomanenkoStud <pavlo.romanenko@nure.ua>
2023-10-31 11:41:48 +0200

    fix: use backend at all pages

--- packages/kite-chat-docs/astro.config.mjs
+++ packages/kite-chat-docs/astro.config.mjs
@@ -116,11 +116,11 @@ export default defineConfig({
     define: {
       __BASE_URL__: base,
       __BRANCH__: JSON.stringify(mode === "test" ? "test" : "main"),
-      __WS_ENDPOINT__: JSON.stringify(WS_ENDPOINT),
-      __BACKEND_PACKAGE_IMPORT__: JSON.stringify(mode === "test" 
+      __WS_ENDPOINT__: WS_ENDPOINT,
+      __BACKEND_PACKAGE_IMPORT__: mode === "test" 
         ? `${base}/kite-chat.js` 
         : "https://cdn.jsdelivr.net/npm/@pragmasoft-ukraine/kite-chat/+esm"
-      ),
+      ,
       //override vite MODE variables
       'import.meta.env.DEV': mode === "development",
       'import.meta.env.PROD': mode !== "development",
