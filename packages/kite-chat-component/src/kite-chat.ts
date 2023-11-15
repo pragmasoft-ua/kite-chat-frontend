1d4677c48eaff7b999a5fffbe0bd621267a3666a
RomanenkoStud <pavlo.romanenko@nure.ua>
2023-11-15 16:26:50 +0200

    fix: add cancel icon

--- packages/kite-chat-component/src/kite-chat.ts
+++ packages/kite-chat-component/src/kite-chat.ts
@@ -336,9 +336,13 @@ export class KiteChatElement extends LitElement {
         <span
           data-cancel
           title="Cancel"
-          class="cursor-pointer rounded-full bg-white bg-opacity-0 py-2 px-2.5 leading-none hover:bg-opacity-30"
+          class="cursor-pointer py-2 px-2.5"
           @click="${this._cancelEdit}"
-          >Cancel</span
+          >
+          <svg xmlns="http://www.w3.org/2000/svg" class="opacity-30 pointer-events-none  h-6 w-6" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 14l-4 -4l4 -4" /><path d="M5 10h11a4 4 0 1 1 0 8h-1" />
+          </svg>
+          </span
         >
       </div>
     `;
