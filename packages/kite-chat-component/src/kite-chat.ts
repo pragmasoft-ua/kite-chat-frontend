26747f718367b64e011c38440e29c3f2845cb96a
RomanenkoStud <pavlo.romanenko@nure.ua>
2023-11-15 15:10:49 +0200

    feat: added edited msg to footer

--- packages/kite-chat-component/src/kite-chat.ts
+++ packages/kite-chat-component/src/kite-chat.ts
@@ -108,6 +108,9 @@ export class KiteChatElement extends LitElement {
   }
 
   @state()
+  private editMessage: KiteMsgElement|null = null;
+
+  @state()
   private sendEnabled = false;
 
   protected anchorController!: AnchorController;
@@ -190,8 +193,7 @@ export class KiteChatElement extends LitElement {
             class="bg-primary-color flex h-12 select-none flex-row items-center justify-between rounded-t p-2 text-secondary-color"
           >
             ${
-              this.selectedMessages.length === 0
-                ? html`
+              this.renderSelected() || html`
                   <h3 class="kite-title flex-1">${this.heading}</h3>
                   <span
                     data-close
@@ -200,40 +202,6 @@ export class KiteChatElement extends LitElement {
                     @click="${this._toggleOpen}"
                     >✕</span
                   >
-                `
-                : html`
-                  <span
-                    data-cancel
-                    title="Cancel"
-                    class="cursor-pointer rounded-full bg-white bg-opacity-0 py-2 px-2.5 leading-none hover:bg-opacity-30"
-                    @click="${this._cancel}"
-                    >✕</span
-                  >
-                  <span class="flex-1">${this.selectedMessages.length} selected</span>
-                  ${
-                    this.editable ? html`
-                      <span
-                        data-edit
-                        title="Edit"
-                        class="cursor-pointer h-full aspect-square rounded-full bg-white bg-opacity-0 py-1 px-1.5 leading-none hover:bg-opacity-30"
-                        @click="${this._edit}"
-                      >
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-full w-full" fill="currentColor" viewBox="0 0 30 30">
-                          <path d="M 22.828125 3 C 22.316375 3 21.804562 3.1954375 21.414062 3.5859375 L 19 6 L 24 11 L 26.414062 8.5859375 C 27.195062 7.8049375 27.195062 6.5388125 26.414062 5.7578125 L 24.242188 3.5859375 C 23.851688 3.1954375 23.339875 3 22.828125 3 z M 17 8 L 5.2597656 19.740234 C 5.2597656 19.740234 6.1775313 19.658 6.5195312 20 C 6.8615312 20.342 6.58 22.58 7 23 C 7.42 23.42 9.6438906 23.124359 9.9628906 23.443359 C 10.281891 23.762359 10.259766 24.740234 10.259766 24.740234 L 22 13 L 17 8 z M 4 23 L 3.0566406 25.671875 A 1 1 0 0 0 3 26 A 1 1 0 0 0 4 27 A 1 1 0 0 0 4.328125 26.943359 A 1 1 0 0 0 4.3378906 26.939453 L 4.3632812 26.931641 A 1 1 0 0 0 4.3691406 26.927734 L 7 26 L 5.5 24.5 L 4 23 z"></path>
-                        </svg>
-                      </span>
-                    ` : null
-                  }
-                  <span
-                    data-delete
-                    title="Delete"
-                    class="cursor-pointer h-full aspect-square rounded-full bg-white bg-opacity-0 py-1 px-1.5 leading-none hover:bg-opacity-30"
-                    @click="${this._delete}"
-                  >
-                  <svg class="h-full w-full" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 50 50">
-                    <path d="M 21 2 C 19.354545 2 18 3.3545455 18 5 L 18 7 L 10.154297 7 A 1.0001 1.0001 0 0 0 9.984375 6.9863281 A 1.0001 1.0001 0 0 0 9.8398438 7 L 8 7 A 1.0001 1.0001 0 1 0 8 9 L 9 9 L 9 45 C 9 46.645455 10.354545 48 12 48 L 38 48 C 39.645455 48 41 46.645455 41 45 L 41 9 L 42 9 A 1.0001 1.0001 0 1 0 42 7 L 40.167969 7 A 1.0001 1.0001 0 0 0 39.841797 7 L 32 7 L 32 5 C 32 3.3545455 30.645455 2 29 2 L 21 2 z M 21 4 L 29 4 C 29.554545 4 30 4.4454545 30 5 L 30 7 L 20 7 L 20 5 C 20 4.4454545 20.445455 4 21 4 z M 11 9 L 18.832031 9 A 1.0001 1.0001 0 0 0 19.158203 9 L 30.832031 9 A 1.0001 1.0001 0 0 0 31.158203 9 L 39 9 L 39 45 C 39 45.554545 38.554545 46 38 46 L 12 46 C 11.445455 46 11 45.554545 11 45 L 11 9 z M 18.984375 13.986328 A 1.0001 1.0001 0 0 0 18 15 L 18 40 A 1.0001 1.0001 0 1 0 20 40 L 20 15 A 1.0001 1.0001 0 0 0 18.984375 13.986328 z M 24.984375 13.986328 A 1.0001 1.0001 0 0 0 24 15 L 24 40 A 1.0001 1.0001 0 1 0 26 40 L 26 15 A 1.0001 1.0001 0 0 0 24.984375 13.986328 z M 30.984375 13.986328 A 1.0001 1.0001 0 0 0 30 15 L 30 40 A 1.0001 1.0001 0 1 0 32 40 L 32 15 A 1.0001 1.0001 0 0 0 30.984375 13.986328 z"></path>
-                  </svg>
-                </span>
                 `
             }
           </header>
@@ -244,66 +212,134 @@ export class KiteChatElement extends LitElement {
               <slot @select=${this.onMsgSelected}></slot>
             </div>
           </main>
-          <footer class="flex items-start gap-1 rounded-b p-2">
-            <label>
-              <input
-                type="file"
-                class="hidden"
-                aria-hidden="true"
-                multiple
-                @change=${this._sendFile}
-              />
+          <footer class="flex flex-col">
+            ${this.renderEdited()}
+            <div class="flex items-start gap-1 rounded-b p-2">
+              <label>
+                <input
+                  type="file"
+                  class="hidden"
+                  aria-hidden="true"
+                  multiple
+                  @change=${this._sendFile}
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="h-6 w-6 cursor-pointer opacity-50 hover:opacity-100"
+                >
+                  <title>Attach file</title>
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13"
+                  />
+                </svg>
+              </label>
+              <textarea
+                required
+                rows="1"
+                autocomplete="on"
+                spellcheck="true"
+                wrap="soft"
+                placeholder="Type a message"
+                class="caret-primary-color w-full max-h-24 min-h-[1.5rem] flex-1 resize-y border-none bg-transparent outline-none"
+                @input=${this._handleEnabled}
+                @keyup=${this._handleKeyUp}
+              ></textarea>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
-                class="h-6 w-6 cursor-pointer opacity-50 hover:opacity-100"
+                class="${classMap({
+                  'opacity-50': this.sendEnabled,
+                  'hover:opacity-100': this.sendEnabled,
+                  'cursor-pointer': this.sendEnabled,
+                  'opacity-30': !this.sendEnabled,
+                  'pointer-events-none': !this.sendEnabled,
+                })} h-6 w-6"
+                @click=${this._sendText}
               >
-                <title>Attach file</title>
+                <title>Send (Ctrl+↩)</title>
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
-                  d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13"
+                  d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
                 />
               </svg>
-            </label>
-            <textarea
-              required
-              rows="1"
-              autocomplete="on"
-              spellcheck="true"
-              wrap="soft"
-              placeholder="Type a message"
-              class="caret-primary-color w-full max-h-24 min-h-[1.5rem] flex-1 resize-y border-none bg-transparent outline-none"
-              @input=${this._handleEnabled}
-              @keyup=${this._handleKeyUp}
-            ></textarea>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="${classMap({
-                'opacity-50': this.sendEnabled,
-                'hover:opacity-100': this.sendEnabled,
-                'cursor-pointer': this.sendEnabled,
-                'opacity-30': !this.sendEnabled,
-                'pointer-events-none': !this.sendEnabled,
-              })} h-6 w-6"
-              @click=${this._sendText}
-            >
-              <title>Send (Ctrl+↩)</title>
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-              />
-            </svg>
+            </div>
           </footer>
         </dialog>
+      </div>
+    `;
+  }
+
+  private renderSelected() {
+    if(this.selectedMessages.length === 0) {
+      return null;
+    }
+    return html`
+      <span
+        data-cancel
+        title="Cancel"
+        class="cursor-pointer rounded-full bg-white bg-opacity-0 py-2 px-2.5 leading-none hover:bg-opacity-30"
+        @click="${this._cancel}"
+        >✕</span
+      >
+      <span class="flex-1">${this.selectedMessages.length} selected</span>
+      ${
+        this.editable ? html`
+          <span
+            data-edit
+            title="Edit"
+            class="cursor-pointer h-full aspect-square rounded-full bg-white bg-opacity-0 py-1 px-1.5 leading-none hover:bg-opacity-30"
+            @click="${this._edit}"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-full w-full" fill="currentColor" viewBox="0 0 30 30">
+              <path d="M 22.828125 3 C 22.316375 3 21.804562 3.1954375 21.414062 3.5859375 L 19 6 L 24 11 L 26.414062 8.5859375 C 27.195062 7.8049375 27.195062 6.5388125 26.414062 5.7578125 L 24.242188 3.5859375 C 23.851688 3.1954375 23.339875 3 22.828125 3 z M 17 8 L 5.2597656 19.740234 C 5.2597656 19.740234 6.1775313 19.658 6.5195312 20 C 6.8615312 20.342 6.58 22.58 7 23 C 7.42 23.42 9.6438906 23.124359 9.9628906 23.443359 C 10.281891 23.762359 10.259766 24.740234 10.259766 24.740234 L 22 13 L 17 8 z M 4 23 L 3.0566406 25.671875 A 1 1 0 0 0 3 26 A 1 1 0 0 0 4 27 A 1 1 0 0 0 4.328125 26.943359 A 1 1 0 0 0 4.3378906 26.939453 L 4.3632812 26.931641 A 1 1 0 0 0 4.3691406 26.927734 L 7 26 L 5.5 24.5 L 4 23 z"></path>
+            </svg>
+          </span>
+        ` : null
+      }
+      <span
+        data-delete
+        title="Delete"
+        class="cursor-pointer h-full aspect-square rounded-full bg-white bg-opacity-0 py-1 px-1.5 leading-none hover:bg-opacity-30"
+        @click="${this._delete}"
+      >
+      <svg class="h-full w-full" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 50 50">
+        <path d="M 21 2 C 19.354545 2 18 3.3545455 18 5 L 18 7 L 10.154297 7 A 1.0001 1.0001 0 0 0 9.984375 6.9863281 A 1.0001 1.0001 0 0 0 9.8398438 7 L 8 7 A 1.0001 1.0001 0 1 0 8 9 L 9 9 L 9 45 C 9 46.645455 10.354545 48 12 48 L 38 48 C 39.645455 48 41 46.645455 41 45 L 41 9 L 42 9 A 1.0001 1.0001 0 1 0 42 7 L 40.167969 7 A 1.0001 1.0001 0 0 0 39.841797 7 L 32 7 L 32 5 C 32 3.3545455 30.645455 2 29 2 L 21 2 z M 21 4 L 29 4 C 29.554545 4 30 4.4454545 30 5 L 30 7 L 20 7 L 20 5 C 20 4.4454545 20.445455 4 21 4 z M 11 9 L 18.832031 9 A 1.0001 1.0001 0 0 0 19.158203 9 L 30.832031 9 A 1.0001 1.0001 0 0 0 31.158203 9 L 39 9 L 39 45 C 39 45.554545 38.554545 46 38 46 L 12 46 C 11.445455 46 11 45.554545 11 45 L 11 9 z M 18.984375 13.986328 A 1.0001 1.0001 0 0 0 18 15 L 18 40 A 1.0001 1.0001 0 1 0 20 40 L 20 15 A 1.0001 1.0001 0 0 0 18.984375 13.986328 z M 24.984375 13.986328 A 1.0001 1.0001 0 0 0 24 15 L 24 40 A 1.0001 1.0001 0 1 0 26 40 L 26 15 A 1.0001 1.0001 0 0 0 24.984375 13.986328 z M 30.984375 13.986328 A 1.0001 1.0001 0 0 0 30 15 L 30 40 A 1.0001 1.0001 0 1 0 32 40 L 32 15 A 1.0001 1.0001 0 0 0 30.984375 13.986328 z"></path>
+      </svg>
+    </span>
+    `;
+  }
+
+  private renderEdited() {
+    if(!this.editMessage) {
+      return null;
+    }
+    const file = this.editMessage.querySelector('kite-file');
+    return html`
+      <div class="flex items-center gap-1 rounded-b p-2">
+        ${!file
+          ? html`<span class="edit-message">${this.editMessage.textContent}</span>`
+          : html`<span 
+            class="edit-message edit-message_file" 
+            @click="${() => this.fileInput.click()}"
+          >${file.name}</span>`
+        }
+        <span
+          data-cancel
+          title="Cancel"
+          class="cursor-pointer rounded-full bg-white bg-opacity-0 py-2 px-2.5 leading-none hover:bg-opacity-30"
+          @click="${this._cancelEdit}"
+          >Cancel</span
+        >
       </div>
     `;
   }
@@ -316,17 +352,26 @@ export class KiteChatElement extends LitElement {
     });
   }
 
+  private _cancelEdit() {
+    if(!this.editMessage) {
+      return;
+    }
+    if(!this.editMessage.querySelector('kite-file')) {
+      this.textarea.value = '';
+      this.textarea.blur();
+    }
+    this.editMessage = null;
+  }
+
   private _edit() {
-    //TODO api call
-    const editedMsg = [...this.messageElements].find((msgElement) => (
+    this.editMessage = [...this.messageElements].find((msgElement) => (
       this.selectedMessages.includes(msgElement.messageId)
-    ));
-    if (editedMsg) {
-      if(!editedMsg.querySelector('kite-file')) {
-        this.textarea.value = editedMsg.innerText;
+    )) ?? null;
+    if (this.editMessage) {
+      this.editMessage.unselect();
+      if(!this.editMessage.querySelector('kite-file')) {
+        this.textarea.value = this.editMessage.innerText;
         this.textarea.focus();
-      } else {
-        this.fileInput.click();
       }
     }
   }
@@ -346,43 +391,36 @@ export class KiteChatElement extends LitElement {
   }
 
   editMsg(msg: KiteMsg) {
-    if(msg.messageId === undefined) {
-      return;
-    }
-    const editedMsg = [...this.messageElements].find((msgElement) => (
-      msgElement.messageId === msg.messageId
-    ));
-    if(!editedMsg) {
+    if(!msg.messageId || !this.editMessage) {
       return;
     }
     const {messageId, timestamp = new Date(), status} = msg;
-    editedMsg.messageId = messageId;
-    editedMsg.timestamp = timestamp;
-    editedMsg.status = status;
-    editedMsg.edited = true;
+    this.editMessage.messageId = messageId;
+    this.editMessage.timestamp = timestamp;
+    this.editMessage.status = status;
+    this.editMessage.edited = true;
     if (isPlaintextMsg(msg)) {
-      editedMsg.innerText = msg.text;
+      this.editMessage.innerText = msg.text;
     } else {
       const {file} = msg;
-      const fileElement = editedMsg.querySelector('kite-file');
+      const fileElement = this.editMessage.querySelector('kite-file');
       fileElement && (fileElement.file = file);
     }
-    editedMsg.unselect();
-    editedMsg.scrollIntoView(false);
+    this.editMessage.scrollIntoView(false);
+    this.editMessage = null;
   }
 
   private _sendText() {
     if (this.textarea.value?.length > 0) {
-      const editMsg = this.selectedMessages.length === 1;
       const message: PlaintextMsg = {
-        messageId: editMsg ? this.selectedMessages[0] : randomStringId(),
+        messageId: this.editMessage ? this.editMessage.messageId : randomStringId(),
         timestamp: new Date(),
         status: MsgStatus.unknown,
         text: this.textarea.value,
-        edited: editMsg,
+        edited: !!this.editMessage,
       };
       if (this._dispatchMsg(message)) {
-        if(editMsg) {
+        if(this.editMessage) {
           this.editMsg(message);
         } else {
           this.appendMsg(message);
@@ -406,21 +444,20 @@ export class KiteChatElement extends LitElement {
   }
 
   private _sendFile(event: Event) {
-    const editMsg = this.selectedMessages.length === 1;
     const target = event.target as HTMLInputElement;
     const numFiles = target.files?.length ?? 0;
     for (let i = 0; i < numFiles; i++) {
       const file = target.files?.item(i);
       if (!file) continue;
       const message: FileMsg = {
-        messageId: editMsg ? this.selectedMessages[0] : randomStringId(),
+        messageId: this.editMessage ? this.editMessage.messageId : randomStringId(),
         timestamp: new Date(),
         status: MsgStatus.unknown,
-        edited: editMsg,
+        edited: !!this.editMessage,
         file,
       };
       if (this._dispatchMsg(message)) {
-        if(editMsg) {
+        if(this.editMessage) {
           this.editMsg(message);
         } else {
           this.appendMsg(message);
