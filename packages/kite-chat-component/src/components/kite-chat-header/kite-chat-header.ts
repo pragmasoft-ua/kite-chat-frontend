776818ca974b1605469cb2f17d01f7cb95f82e46
RomanenkoStud <pavlo.romanenko@nure.ua>
2023-12-12 16:35:18 +0200

    fix: change cancel icon

--- packages/kite-chat-component/src/components/kite-chat-header/kite-chat-header.ts
+++ packages/kite-chat-component/src/components/kite-chat-header/kite-chat-header.ts
@@ -73,9 +73,11 @@ export class KiteChatHeaderElement extends LitElement {
       <span
         data-cancel
         title="Cancel"
-        class="cursor-pointer rounded-full bg-white bg-opacity-0 py-2 px-2.5 leading-none hover:bg-opacity-30"
+        class="cursor-pointer rounded-full bg-white bg-opacity-0 p-1 leading-none hover:bg-opacity-30"
         @click="${this._handleCancel}"
-        >✕</span
+        ><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-left" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M5 12l6 6" /><path d="M5 12l6 -6" />
+        </svg></span
       >
       <span class="flex-1">${this.selectedElementsCount} selected</span>
         ${
